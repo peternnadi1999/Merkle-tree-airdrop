751f3aa2ac3b04869db51401ae5a166b6f4dca83
petnacode <peternnadi1999@gmail.com>
2024-10-01 23:45:28 +0100

    deployed the merkletress contract

--- ignition/modules/MerkleAirdrop.ts
+++ ignition/modules/MerkleAirdrop.ts
@@ -1,9 +1,11 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const tokenAddress = "0x7B4982e1F7ee384F206417Fb851a1EB143c513F9";
-const merkleRoot = "0xee9d6378d7566ee489933c1b4a7b34843f5886bb725f41dd3932858c6a52d630";
 const MerkleAirdropModule = buildModule("MerkleAirdropModule", (m) => {
-  const merkleAirdrop = m.contract("merkleAirdrop", [tokenAddress, merkleRoot]);
+  
+  const tokenAddress = "0x7B4982e1F7ee384F206417Fb851a1EB143c513F9";
+  const merkleRoot = "0xee9d6378d7566ee489933c1b4a7b34843f5886bb725f41dd3932858c6a52d630";
+  
+  const merkleAirdrop = m.contract("MerkleAirdrop", [tokenAddress, merkleRoot]);
 
   return { merkleAirdrop };
 });
